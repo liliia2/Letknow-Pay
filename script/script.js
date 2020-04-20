220c99de444449590bc7863440661a498ba36725
liliia2 <besenoklili92@gmail.com>
2020-04-20 14:57:11 +0300

    added adaptive for main screen

--- script/script.js
+++ script/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    console.log('Hi');
+    console.log('Hi');  
 });
+
+function openMenu() {
+    document.getElementById('mobile-menu').classList.remove('inactive'); 
+    document.getElementById('mobile-menu').classList.add('active'); 
+    document.body.classList.add('locked');
+}
+
+function closeMenu() {
+    document.getElementById('mobile-menu').classList.remove('active'); 
+    document.getElementById('mobile-menu').classList.add('inactive'); 
+    document.body.classList.remove('locked');
+}
