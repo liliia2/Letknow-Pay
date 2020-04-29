9d4d72b5064e4ccdbb7f6a58678d7142d031c234
liliia2 <besenoklili92@gmail.com>
2020-04-29 18:44:19 +0300

    added open_banking page

--- script/slider.js
+++ script/slider.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     let currentSlideIndex = 0;
     
-    $('.slider .all-slides').slick({
+    $('.slider').slick({
         speed: 600,
         fade: true,
         cssEase: 'linear',
@@ -12,27 +12,27 @@ $(document).ready(function () {
         autoplaySpeed: 100000
     });
 
-    $('.slider .all-slides').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    $('.slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
         $('.all-slides').addClass("slick-before-change");
     });
 
-    $('.slider .all-slides').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $('.slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
         currentSlideIndex = currentSlide;
     });
 
     $(".nav1").on("click", function () {
-        $('.slider .all-slides')[0].slick.slickGoTo(0);
+        $('.slider')[0].slick.slickGoTo(0);
     });
 
     $(".nav2").on("click", function () {
-        $('.slider .all-slides')[0].slick.slickGoTo(1);
+        $('.slider')[0].slick.slickGoTo(1);
     });
 
     $(".slick-prev").on("click", function () {
-        $('.slider .all-slides')[0].slick.slickGoTo(0);
+        $('.slider')[0].slick.slickGoTo(0);
     });
     
     $(".slick-next").on("click", function () {
-        $('.slider .all-slides')[0].slick.slickGoTo(1);
+        $('.slider')[0].slick.slickGoTo(1);
     });
 });
